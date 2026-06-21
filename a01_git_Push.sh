5c3f32910a3741a54c47519a8c26f01abf51d94c
GOUMGUE SEVAN <gmgsevan@gmail.com>
2026-06-21 09:08:33 -0400

    file updated

--- a01_git_Push.sh
+++ a01_git_Push.sh
@@ -1,15 +1,7 @@
 #!/bin/bash 
 
-git add README.md
-git commit -m "README.md added"
-
-git add .gitignore
-git commit -m ".gitignore added"
-
-
-git add a01_git_Push.sh
-git commit -m "a01_git_Push.sh added"
-
+git add .
+git commit -m "file updated"
 
 
 git push 
