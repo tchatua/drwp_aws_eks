59e2dad7cd6d854a0a7334c842acf5b816cbbc30
GOUMGUE SEVAN <gmgsevan@gmail.com>
2026-06-21 08:56:26 -0400

    s01_delete_dataplane_eksaddon_vpce_aws_infra added

---
+++ s01_delete_dataplane_eksaddon_vpce_aws_infra.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e  # Exit immediately if any command fails
+
+echo "------------------------------------------"
+echo "STEP1: Deleting dataplane using Terraform"
+echo "------------------------------------------"
+
+cd f02_aws_dataplane_terraform_deployment/ || { echo "ERROR: EKS module directory not found"; exit 1; }
+
+terraform apply -destroy -auto-approve
+
+
+echo "------------------------------------------"
+echo "STEP1: Deleting EKS Cluster using Terraform"
+echo "------------------------------------------"
+
+cd ../f01_EKS_Terraform_Manifest_with_Addons/b06_EKS_Cluster_with_Addons || { echo "ERROR: VPC module directory not found"; exit 1; }
+
+
+terraform apply -destroy -auto-approve
+
+echo "----------------------------------"
+echo "STEP1: Deleting VPC using Terraform"
+echo "----------------------------------"
+
+cd ../b04_VPC_Module
+
+terraform apply -destroy -auto-approve
+
+
+echo "-------------------------------------------"
+echo "All Terraform steps completed successfully!"
+echo "-------------------------------------------"
+
+
