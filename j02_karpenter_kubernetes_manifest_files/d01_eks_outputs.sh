a71bbd39de27619f8f93d0224e40db418d6b385e
Arristide Tchatua <tchattua@gmail.com>
2026-07-08 07:33:01 -0400

    Karpenter Kubernetes Manifest Files Added

---
+++ j02_karpenter_kubernetes_manifest_files/d01_eks_outputs.sh
@@ -0,0 +1,363 @@
+terraform output
+aws_eks_pod_identity_association_id = "a-xxtzd0wpjxuzn5ssm"
+ebs_csi_addon_arn = "arn:aws:eks:us-east-2:088354478627:addon/eks-control-plane/aws-ebs-
+csi-driver/8ccf9f9e-2b0e-e535-e745-8bea0b79e300"
+ebs_csi_addon_default_version = "v1.62.0-eksbuild.1"
+ebs_csi_addon_id = "eks-control-plane:aws-ebs-csi-driver"
+ebs_csi_addon_latest_version = "v1.62.0-eksbuild.1"
+ebs_csi_iam_role_arn = "arn:aws:iam::088354478627:role/south-jersey-eks-tchatua-dev-ebs-csi-iam-role"
+ebs_csi_pod_identity_association_arn = "arn:aws:eks:us-east-2:088354478627:podidentityassociation/eks-control-plane/a-zwbzrgkauiac0fjhn"
+eks_cluster_arn = "arn:aws:eks:us-east-2:088354478627:cluster/eks-control-plane"
+eks_cluster_certificate_authority_data = "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSURCVENDQWUyZ0F3SUJBZ0lJSXcwQWRxWENta2N3RFFZSktvWklodmNOQVFFTEJRQXdGVEVUTUJFR0ExVUUKQXhNS2EzVm
+laWEp1WlhSbGN6QWVGdzB5TmpBM01EZ3dNRFE1TVRSYUZ3MHpOakEzTURVd01EVTBNVFJhTUJVeApFekFSQmdOVkJBTVRDbXQxWW1WeWJtVjBaWE13Z2dFaU1BMEdDU3FHU0liM0RRRUJBUVVBQTRJQkR3QXdnZ0VLCkFvSUJBUURGdE
+FjS25weHpUZUMxWHZyL0txS0ViZ3B4ZVNQcm40Y3lOSXdvUWpPYXA2Z2pQRnN2TjlhRGhzMnMKVmFjMkU5cVE1V1ZiSnhwNXpqNWZXNitNYVJ2aGx3eFVXYThvY3RJcDB6UjRmaE01RmpYOGhIQ1RQUW1CVkFvWgpjWnFLZGtTdDFTTmlBbFJwWVVVR3pNZzlZb3o1VDdrZ3NxUkpEUmx1Z3A0YTJ2WUQrNHlIVnY5R1d1d05FOHlqCkQ3S201RGFzN2M2a2
+9vRkhudzFORlRFMlhMeWlKc0wvNlFXVCtLNUxrZmlPSzF5Sm8vQzhEaVRzR0VDV2xvVGoKeFdQUk8vR2YrY09pSE8xQzV2UkVYNnJmUmN5T0ZZK044ejJ2VUdMNXJ1azNsRmZqN3dxaHdBZjlVVVdxTjBuRwpMWkVoTzlWZ2VSYWpaWk4yOFBLclg4d1NxMDBoQWdNQkFBR2pXVEJYTUE0R0ExVWREd0VCL3dRRUF3SUNwREFQCkJnTlZIUk1CQWY4RUJUQURBUUgvTUIwR0ExVWREZ1FXQkJRRnFDeC9aKzhwY3d2TEZvd0ZvSlp0M0JqSVlUQVYKQmdOVkhSRUVEakFNZ2dwcmRXSmxjbTVsZEdWek1BMEdDU3FHU0liM0RRRUJDd1VBQTRJQkFRQU5RRnVYV0hpbgpyWWdobHVEQnlzbmMwZW92RFJJcjFvY2VtalRpT1NkRTJHRlNJOEpSZFRDd0czTkU1RTVGd2o4UHBMc1l4NU1sCnZYbWlmNUZIdFRrN24yVlJ1OWkvbU9URTdDcDZ1eUpwMmVhR3l2dGM4bmhkZlJpN01FbnRMTHZNZEg5LytwangKZG1odmZPcjFjMU1RSnVxenF4cEZEU0xyRFM2WnA4aWY4aTZZUVhPb3J3QTBZRUdzdGtTemhJL0xzaUd1SDIrOQp3VElkU0kwcjhLV2FoZStLZlc0UVRVcExGUUpqZEc5T0hBNFRvMktLNnZqUWI2ZGpVK0U1eUhzY01qQzRiS1Q2CmR5aHBPWjlETWgxQkE5WTBDaUNpVnRvcmRKRDdHcW8yMlVLSmdaRzBjd3VMREtWYklYejBLSSs0SUNBNVMybjcKUmtTUWlmQkJWdzNSCi0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K"
+eks_cluster_endpoint = "https://5865000538DA3CD8BA8376DBCDDADD24.gr7.us-east-2.eks.amazonaws.com"
+eks_cluster_id = "eks-control-plane"
+eks_cluster_name = "eks-control-plane"
+eks_cluster_security_group_id = "sg-0896cc29e97573ea2"
+eks_cluster_status = "ACTIVE"
+eks_cluster_version = "1.34"
+eks_node_instance_role_arn = "arn:aws:iam::088354478627:role/south-jersey-eks-tchatua-dev-eks-nodegroup-role"
+external_dns_addon_arn = "arn:aws:eks:us-east-2:088354478627:addon/eks-control-plane/external-dns/42cf9f9e-2b0e-118b-e501-4e73923e1a60"
+external_dns_addon_version = "v0.21.0-eksbuild.5"
+external_dns_role_arn = "arn:aws:iam::088354478627:role/south-jersey-eks-tchatua-dev-external-dns-role"
+externaldns_addon_id = "eks-control-plane:external-dns"
+helm_aws_secrets_provider_metadata = {
+  "app_version" = ""
+  "chart" = "secrets-store-csi-driver-provider-aws"
+  "first_deployed" = 1783472370
+  "last_deployed" = 1783472370
+  "name" = "secrets-provider-aws"
+  "namespace" = "kube-system"
+  "notes" = ""
+  "revision" = 1
+  "values" = "{\"secrets-store-csi-driver\":{\"install\":false}}"
+  "version" = "3.1.1"
+}
+helm_lbc_metadata = {
+  "app_version" = "v3.4.1"
+  "chart" = "aws-load-balancer-controller"
+  "first_deployed" = 1783472348
+  "last_deployed" = 1783472348
+  "name" = "aws-load-balancer-controller"
+  "namespace" = "kube-system"
+  "notes" = <<-EOT
+  AWS Load Balancer controller installed!
+
+  EOT
+  "revision" = 1
+  "values" = "{\"clusterName\":\"eks-control-plane\",\"region\":\"us-east-2\",\"serviceAccount\":{\"create\":true,\"name\":\"aws-load-balancer-controller\"},\"vpcId\":\"vpc-0e470907c96317802\"}"
+  "version" = "3.4.1"
+}
+helm_secrets_store_csi_driver_metadata = {
+  "app_version" = "1.6.0"
+  "chart" = "secrets-store-csi-driver"
+  "first_deployed" = 1783472347
+  "last_deployed" = 1783472347
+  "name" = "csi-secrets-store"
+  "namespace" = "kube-system"
+  "notes" = <<-EOT
+  The Secrets Store CSI Driver is getting deployed to your cluster.
+
+  To verify that Secrets Store CSI Driver has started, run:
+
+    kubectl --namespace=kube-system get pods -l "app=secrets-store-csi-driver"
+
+  Now you can follow these steps https://secrets-store-csi-driver.sigs.k8s.io/getting-started/usage.html
+  to create a SecretProviderClass resource, and a deployment using the SecretProviderClass.
+
+  EOT
+  "revision" = 1
+  "values" = "{\"syncSecret\":{\"enabled\":true}}"
+  "version" = "1.6.0"
+}
+lbc_iam_policy = <<EOT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:CreateServiceLinkedRole"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:AWSServiceName": "elasticloadbalancing.amazonaws.com"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeAccountAttributes",
+                "ec2:DescribeAddresses",
+                "ec2:DescribeAvailabilityZones",
+                "ec2:DescribeInternetGateways",
+                "ec2:DescribeVpcs",
+                "ec2:DescribeVpcPeeringConnections",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeInstances",
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:DescribeTags",
+                "ec2:GetCoipPoolUsage",
+                "ec2:DescribeCoipPools",
+                "ec2:GetSecurityGroupsForVpc",
+                "ec2:DescribeIpamPools",
+                "ec2:DescribeRouteTables",
+                "elasticloadbalancing:DescribeLoadBalancers",
+                "elasticloadbalancing:DescribeLoadBalancerAttributes",
+                "elasticloadbalancing:DescribeListeners",
+                "elasticloadbalancing:DescribeListenerCertificates",
+                "elasticloadbalancing:DescribeSSLPolicies",
+                "elasticloadbalancing:DescribeRules",
+                "elasticloadbalancing:DescribeTargetGroups",
+                "elasticloadbalancing:DescribeTargetGroupAttributes",
+                "elasticloadbalancing:DescribeTargetHealth",
+                "elasticloadbalancing:DescribeTags",
+                "elasticloadbalancing:DescribeTrustStores",
+                "elasticloadbalancing:DescribeListenerAttributes",
+                "elasticloadbalancing:DescribeCapacityReservation"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "cognito-idp:DescribeUserPoolClient",
+                "acm:ListCertificates",
+                "acm:DescribeCertificate",
+                "iam:ListServerCertificates",
+                "iam:GetServerCertificate",
+                "waf-regional:GetWebACL",
+                "waf-regional:GetWebACLForResource",
+                "waf-regional:AssociateWebACL",
+                "waf-regional:DisassociateWebACL",
+                "wafv2:GetWebACL",
+                "wafv2:GetWebACLForResource",
+                "wafv2:AssociateWebACL",
+                "wafv2:DisassociateWebACL",
+                "shield:GetSubscriptionState",
+                "shield:DescribeProtection",
+                "shield:CreateProtection",
+                "shield:DeleteProtection"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AuthorizeSecurityGroupIngress",
+                "ec2:RevokeSecurityGroupIngress"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateSecurityGroup"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateTags"
+            ],
+            "Resource": "arn:aws:ec2:*:*:security-group/*",
+            "Condition": {
+                "StringEquals": {
+                    "ec2:CreateAction": "CreateSecurityGroup"
+                },
+                "Null": {
+                    "aws:RequestTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateTags",
+                "ec2:DeleteTags"
+            ],
+            "Resource": "arn:aws:ec2:*:*:security-group/*",
+            "Condition": {
+                "Null": {
+                    "aws:RequestTag/elbv2.k8s.aws/cluster": "true",
+                    "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AuthorizeSecurityGroupIngress",
+                "ec2:RevokeSecurityGroupIngress",
+                "ec2:DeleteSecurityGroup"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Null": {
+                    "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:CreateLoadBalancer",
+                "elasticloadbalancing:CreateTargetGroup"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Null": {
+                    "aws:RequestTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:CreateListener",
+                "elasticloadbalancing:DeleteListener",
+                "elasticloadbalancing:CreateRule",
+                "elasticloadbalancing:DeleteRule"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:AddTags",
+                "elasticloadbalancing:RemoveTags"
+            ],
+            "Resource": [
+                "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*",
+                "arn:aws:elasticloadbalancing:*:*:loadbalancer/net/*/*",
+                "arn:aws:elasticloadbalancing:*:*:loadbalancer/app/*/*"
+            ],
+            "Condition": {
+                "Null": {
+                    "aws:RequestTag/elbv2.k8s.aws/cluster": "true",
+                    "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:AddTags",
+                "elasticloadbalancing:RemoveTags"
+            ],
+            "Resource": [
+                "arn:aws:elasticloadbalancing:*:*:listener/net/*/*/*",
+                "arn:aws:elasticloadbalancing:*:*:listener/app/*/*/*",
+                "arn:aws:elasticloadbalancing:*:*:listener-rule/net/*/*/*",
+                "arn:aws:elasticloadbalancing:*:*:listener-rule/app/*/*/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:ModifyLoadBalancerAttributes",
+                "elasticloadbalancing:SetIpAddressType",
+                "elasticloadbalancing:SetSecurityGroups",
+                "elasticloadbalancing:SetSubnets",
+                "elasticloadbalancing:DeleteLoadBalancer",
+                "elasticloadbalancing:ModifyTargetGroup",
+                "elasticloadbalancing:ModifyTargetGroupAttributes",
+                "elasticloadbalancing:DeleteTargetGroup",
+                "elasticloadbalancing:ModifyListenerAttributes",
+                "elasticloadbalancing:ModifyCapacityReservation",
+                "elasticloadbalancing:ModifyIpPools"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Null": {
+                    "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:AddTags"
+            ],
+            "Resource": [
+                "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*",
+                "arn:aws:elasticloadbalancing:*:*:loadbalancer/net/*/*",
+                "arn:aws:elasticloadbalancing:*:*:loadbalancer/app/*/*"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "elasticloadbalancing:CreateAction": [
+                        "CreateTargetGroup",
+                        "CreateLoadBalancer"
+                    ]
+                },
+                "Null": {
+                    "aws:RequestTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:RegisterTargets",
+                "elasticloadbalancing:DeregisterTargets"
+            ],
+            "Resource": "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:SetWebAcl",
+                "elasticloadbalancing:ModifyListener",
+                "elasticloadbalancing:AddListenerCertificates",
+                "elasticloadbalancing:RemoveListenerCertificates",
+                "elasticloadbalancing:ModifyRule",
+                "elasticloadbalancing:SetRulePriorities"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+
+EOT
+lbc_iam_policy_arn = "arn:aws:iam::088354478627:policy/south-jersey-eks-tchatua-dev-AWSLoadBalancerControllerIAMPolicy"
+lbc_iam_role_arn = "arn:aws:iam::088354478627:role/south-jersey-eks-tchatua-dev-lbc-iam-role"
+lbc_pod_identity_association_arn = "arn:aws:eks:us-east-2:088354478627:podidentityassociation/eks-control-plane/a-6vqg0kbsezdc0r7oz"
+pod_identity_agent_eks_addon_arn = "arn:aws:eks:us-east-2:088354478627:addon/eks-control-plane/eks-pod-identity-agent/4acf9f9d-fb8b-15b3-e007-814dde37240e"
+pod_identity_agent_eks_addon_default_version = {
+  "addon_name" = "eks-pod-identity-agent"
+  "id" = "eks-pod-identity-agent"
+  "kubernetes_version" = "1.34"
+  "most_recent" = false
+  "region" = "us-east-2"
+  "version" = "v1.3.10-eksbuild.3"
+}
+pod_identity_agent_eks_addon_id = "eks-control-plane:eks-pod-identity-agent"
+pod_identity_agent_eks_addon_latest_version = {
+  "addon_name" = "eks-pod-identity-agent"
+  "id" = "eks-pod-identity-agent"
+  "kubernetes_version" = "1.34"
+  "most_recent" = true
+  "region" = "us-east-2"
+  "version" = "v1.3.10-eksbuild.3"
+}
+private_node_group_name = "eks-control-plane-eks-node-group"
+private_subnet_ids = [
+  "subnet-0739442a734c80426",
+  "subnet-0a045721ae0be0436",
+  "subnet-037489b0725fabe93",
+]
+public_subnet_ids = [
+  "subnet-0c4b3fecc1d2e3a5a",
+  "subnet-02634dfd14249af77",
+  "subnet-02d05e7dd6c54c8e8",
+]
+to_configure_kubectl_command = "aws eks --region us-east-2 update-kubeconfig --name eks-control-plane"
+vpc_id = "vpc-0e470907c96317802"
+
